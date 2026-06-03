ad83801d1fd1427072f40c8b28fb601d38a6f3b5
Jay Herron <NeedleInAJayStack@protonmail.com>
2026-06-02 21:50:19 -0600

    chore: Update @grafana/create-plugin

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'eslint/config';
+import baseConfig from './.config/eslint.config.mjs';
+
+export default defineConfig([
+  {
+    ignores: [
+      '**/logs',
+      '**/*.log',
+      '**/npm-debug.log*',
+      '**/yarn-debug.log*',
+      '**/yarn-error.log*',
+      '**/node_modules/',
+      '**/pids',
+      '**/*.pid',
+      '**/*.seed',
+      '**/*.pid.lock',
+      '**/lib-cov',
+      '**/coverage',
+      '**/dist/',
+      '**/artifacts/',
+      '**/work/',
+      '**/ci/',
+      '**/e2e-results/',
+      '**/cypress/videos',
+      '**/cypress/report.json',
+      '**/.idea',
+      '**/.eslintcache',
+      'pkg/.env',
+      '**/release/',
+      '**/.DS_Store',
+    ],
+  },
+  ...baseConfig,
+]);
